7c662b5e84e0ca29202c6897faaa086bace8746d
Sameer Negi <sameer.negi17@gmail.com>
2023-01-21 16:24:31 +0100

    Is unique solution

--- Array_Strings/is_unique.py
+++ Array_Strings/is_unique.py
@@ -6,12 +6,14 @@ cannot use additional data structures?
 from test_utils.helper_functions import verify_output
 
 def is_unique(data):
-    pass
+    temp_set = set(data)
+    print("Tempory set ", temp_set)
 
-
-
-
-
+    if len(temp_set) == len(data):
+        return True
+    else:
+        return False
+    
 if __name__ == "__main__":
     verify_output(True,is_unique("asewrt"))
     verify_output(False,is_unique("aseer"))
